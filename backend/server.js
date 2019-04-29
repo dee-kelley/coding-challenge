3330f0c6f18de407f74ba908e2713f3567fb8ac7
Deirdre <deirdre@Deirdres-MacBook-Air.local>
2019-04-29 12:28:16 -0600

    changed port

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 
 //const PORT = 4000;
